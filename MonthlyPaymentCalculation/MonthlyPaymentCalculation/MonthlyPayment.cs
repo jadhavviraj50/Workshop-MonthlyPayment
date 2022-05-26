426efdc3c49d2c458bfa64817dd0f3bf0ebfcc3e
Viraj <jadhavviraj50@gmail.com>
2022-05-26 13:52:39 +0530

    [Viraj]-Added Calculate Monthly Payment file

---
+++ MonthlyPaymentCalculation/MonthlyPaymentCalculation/MonthlyPayment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPaymentCalculation
+{
+    public class MonthlyPayment
+    {
+        public void comp()
+        {
+            double P, R, n, r, ci;
+            int Y;
+
+            Console.WriteLine("Enter Principal");
+            P = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter Year");
+            Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter rate");
+            R = Convert.ToDouble(Console.ReadLine());
+
+            n = 12 * Y;
+            r = R / (12 * 100);
+            ci = (P * r) / (1 - Math.Pow((1 + r), -n));
+
+            Console.WriteLine("Monthly Payment have to pay over years : " + ci);
+
+        }
+    }
+}
---
+++ MonthlyPaymentCalculation/MonthlyPaymentCalculation/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MonthlyPaymentCalculation;
+ public class program
+ {
+    static void Main(String[] args)
+    {
+        MonthlyPayment calculate = new MonthlyPayment();
+        calculate.comp();
+
+    }
+ }
